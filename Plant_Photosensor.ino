d301f5c85a6cf19b0916da5d2b9513a4c36cb288
napstercc <conradchamerski@gmail.com>
2012-10-29 00:37:45 -0400

    Initial code
    Code needs some fixes in:
    1- plant timer
    2- Fan timer
    3- adjust moist sensor value
    
    use blink w/o delay code

---
+++ Plant_Photosensor.ino
@@ -0,0 +1,120 @@
+
+/* Plant System
+
+Checks Temperature/Humidity/Moist/Light
+Activates light when needed/plays sound when Dry soil
+
+TODO: Upload to SQL server and website!
+2012/10/7
+ Code by Conrad Chamerski
+*/
+
+#include "DHT.h"
+#include "pitches.h"
+#define DHTPIN 2     // what pin we're connected to
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+
+DHT dht(DHTPIN, DHTTYPE);
+
+//variables
+unsigned long elapsed;
+int lightData [48];
+int moistData [48];
+float tempData [48];
+float tempHumitidy [48];
+ float hours,minutes,seconds,ms;
+  unsigned long over;
+  int fanRelay = 6;
+
+ 
+
+int timeCounter=0;
+int lightRelay = 5; 
+
+void setup() {
+  Serial.begin(9600);   
+    Serial.println("Program Start!");
+  dht.begin();
+  pinMode(fanRelay, OUTPUT);     
+  
+}
+
+int melody[] = {
+  NOTE_C4, NOTE_G3,NOTE_G3, NOTE_A3, NOTE_G3,0, NOTE_B3, NOTE_C4};
+
+int noteDurations[] = {
+  4, 8, 8, 4,4,4,4,4 };
+
+int photocellPin = 0;     // the cell and 10K pulldown are connected to a0  ANALOG
+int photocellReading;     // the analog reading from the sensor divider
+
+void loop() {
+  
+  photocellReading = analogRead(photocellPin);  
+  Serial.print("Analog reading = ");
+  Serial.println(photocellReading);     // the raw analog reading
+ 
+  delay(100);
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(t) || isnan(h)) {
+    Serial.println("Failed to read from DHT");
+  } else {
+    Serial.print("Humidity: "); 
+    Serial.print(h);
+    Serial.print(" %\t");
+    Serial.print("Temperature: "); 
+    Serial.print(t);
+    Serial.println(" *C");
+  }
+  
+  int moistSensor;
+  moistSensor = analogRead(5);
+  Serial.print("Moist reading = ");
+  Serial.println(moistSensor); 
+
+   elapsed=millis();
+  hours=int(elapsed/3600000);
+  over=elapsed%3600000;
+  minutes=int(over/60000);
+  over=over%60000;
+  seconds=int(over/1000);
+  ms=over%1000;
+  
+    if ((int)minutes%30==0)// && (int)minutes!= 0)
+  {
+         digitalWrite(fanRelay, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(120000);               // wait for a second
+  digitalWrite(fanRelay, LOW);    // turn the LED off by making the voltage LOW
+  }
+  
+
+  if (moistSensor < 850 && (int)minutes%5==0 && (int)minutes!= 0)  // Change to Good Value
+  {
+   for (int thisNote = 0; thisNote < 8; thisNote++) {
+    int noteDuration = 1000/noteDurations[thisNote];
+    tone(8, melody[thisNote],noteDuration);
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    noTone(8);
+  }
+  
+  
+  if ( photocellReading > 600 )
+  {
+    digitalWrite(lightRelay, HIGH);
+  }
+  
+ if( (int)hours%18 == 0 && hours != 0 )
+ {
+   int currentTime = hours + 6;
+   while(int(millis()/3600000) < currentTime)
+   {
+     digitalWrite(lightRelay, HIGH);
+   }
+ }
+  
+
+  }
+}
